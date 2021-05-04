b3ad75379865e8ec9973f03b0704bd8324face17
Bryan Civils <hermanbciv@gmail.com>
2021-05-03 23:27:57 -0400

    Fix time logic for textarea background colors

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,23 +1,33 @@
 var textArea = $('.textarea');
-
-// var ids = ['$(c-todo-r-a)', '$(c-todo-r-b)', '$(c-todo-r-c)', '$(c-todo-r-d)', '$(c-todo-r-e)', '$(c-todo-r-f)', '$(c-todo-r-g)', '$(c-todo-r-h)', '$(c-todo-r-i)'];
-var hours = [9, 10, 11, 12, 13, 14, 15, 16, 16, 17];
-console.log(`CURRENT HOUR VALUE:  ${tempGetHour = moment().get('hour')}`);
+var saveButton = $('.saveBtn');
+var idTextArea = {
+    0: $('#c-todo-r-a'),
+    1: $('#c-todo-r-b'),
+    2: $('#c-todo-r-c'),
+    3: $('#c-todo-r-d'),
+    4: $('#c-todo-r-e'),
+    5: $('#c-todo-r-f'),
+    6: $('#c-todo-r-g'),
+    7: $('#c-todo-r-h'),
+    8: $('#c-todo-r-i')
+};
+var hours = [9, 10, 11, 12, 13, 14, 15, 16, 17];
+var memory = [];
 
 dateTodayHeader();
 timeLabelColorEval();
 
 
-
-
 function timeLabelColorEval() {
+    
     for (let i = 0; i < hours.length; i++) {
+        var idTextAreaObjects = idTextArea[i];
         if (moment().get('hour') < hours[i]) {
-            textArea.addClass('future');
+            idTextAreaObjects.addClass('future');
         } else if (moment().get('hour') > hours[i]) {
-            textArea.addClass('past');
+            idTextAreaObjects.addClass('past');
         } else if (moment().get('hour') === hours[i]) {
-            textArea.addClass('present');
+            idTextAreaObjects.addClass('present');
         }
     };
 };
@@ -28,3 +38,11 @@ function dateTodayHeader() {
     dateToday = moment().format("dddd, MMMM Do YYYY");
     headerDateSpace.text(dateToday);
 };
+
+function saveDescription() {
+
+}
+
+saveButton.on('click', function() {
+
+})
